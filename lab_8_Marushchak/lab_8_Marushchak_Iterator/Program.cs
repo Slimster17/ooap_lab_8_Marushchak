a92f359950ebcbb079019708340ff6e112fc4704
Slimster17 <narser17@gmail.com>
2024-02-17 11:56:21 +0200

    Initial commit

---
+++ lab_8_Marushchak/lab_8_Marushchak_Iterator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_8_Marushchak
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        
+        public string Status { get; set; }
+
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+    }
+    
+    class SelfEmployee : Employee
+    {
+        public SelfEmployee(string name, double salary) : base(name, salary)
+        {
+            Status = "Self employeed";
+        }
+        
+    }
+    
+    class SalariedEmployee: Employee
+    {
+        public SalariedEmployee(string name, double salary) : base(name, salary)
+        {
+            Status = "Salaried";
+        }
+    }
+    
+    class UnemployedEmployee : Employee
+    {
+        public UnemployedEmployee(string name) : base(name, 0)
+        {
+            Status = "Unemployeed";
+        }
+    }
+
+    class EmployeeIterator
+    {
+        private List<Employee> _employees;
+        private int _currentIndex;
+
+        public EmployeeIterator(List<Employee> employees)
+        {
+            _employees = employees;
+            _employees.Sort((emp1,emp2) => emp1.Salary.CompareTo(emp2.Salary));
+            _currentIndex = 0;
+        }
+
+        public bool HasNext()
+        {
+            return _currentIndex < _employees.Count;
+        }
+
+        public Employee Next()
+        {
+            if (!HasNext())
+            {
+                throw new InvalidOperationException("No more elements in the collection");
+            }
+
+            Employee nextEmployee = _employees[_currentIndex];
+            _currentIndex++;
+            return nextEmployee;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new SelfEmployee("Ivan", 1700),
+                new SelfEmployee("Taras", 1250),
+                new SalariedEmployee("Mykola", 3500),
+                new UnemployedEmployee("Petro")
+            };
+
+            EmployeeIterator employeeIterator = new EmployeeIterator(employees);
+
+            while (employeeIterator.HasNext())
+            {
+                Employee employee = employeeIterator.Next();
+                Console.WriteLine($"Name: {employee.Name}; Salary: {employee.Salary}; Status: {employee.Status}");
+            }
+        }
+    }
+}
---
+++ lab_8_Marushchak/lab_8_Mediator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab_8_Mediator
+{
+    interface IChatMediator
+    {
+        void SendMessage(string message, User sender, string receiverName);
+    }
+
+    abstract class User
+
+    {
+        protected IChatMediator _chatMediator;
+        public string Name { get; }
+
+        protected User(IChatMediator chatMediator,string name)
+        {
+            _chatMediator = chatMediator;
+            Name = name;
+        }
+
+        public abstract void ReceiveMessage(string message);
+    }
+    
+    class ChatMediator : IChatMediator
+    {
+        private List<User> _users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+        
+        public void SendMessage(string message, User sender, string receiverName)
+        {
+           User receiver = _users.Find(usr => usr.Name== receiverName);
+
+           if (receiver != null)
+           {
+               receiver.ReceiveMessage($"Message from: {sender.Name}, message: {message}");
+           }
+           else
+           {
+               Console.WriteLine($"User *{receiverName}* is not found");
+           }
+        }
+    }
+    
+    class ConctreateUser : User
+    {
+        public ConctreateUser(IChatMediator chatMediator, string name) : base(chatMediator, name)
+        {
+        }
+
+        public override void ReceiveMessage(string message)
+        {
+            Console.WriteLine($"{Name} received message; {message}");
+        }
+
+        public void SendMessage(string message, string receiverName)
+        {
+            _chatMediator.SendMessage(message,this,receiverName);
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ChatMediator mediator = new ChatMediator();
+
+            ConctreateUser user1 = new ConctreateUser(mediator, "Ivan");
+            ConctreateUser user2 = new ConctreateUser(mediator, "Petro");
+
+            mediator.AddUser(user1);
+            mediator.AddUser(user2);
+
+            user1.SendMessage("Hello, Boris","Boris");
+            user1.SendMessage("Hello, Petro", "Petro");
+            user2.SendMessage("Hi, Ivan", "Ivan");
+        }
+    }
+}
